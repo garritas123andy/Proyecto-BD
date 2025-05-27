dd3621324d2eb24c7260887f804008a657b84440
cris1059 <149038713+cris1059@users.noreply.github.com>
2025-05-26 21:33:24 -0600

    Cambios generales

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import PerroForm from './templates/PerroForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Centro de Adopcion Garritas</h1>
+        <nav>
+          <Link to="/">Inicio</Link>
+          <Link to="/perros">Perros</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/perros" element={<PerroForm/>}></Route>
+        </Routes>
+
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Este es el Home</h1>
+    </div>
+  )
 }
 
 export default App;
---
+++ frontend/src/templates/PerroForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PerroForm() {
+    
+
+    return (
+        <div>
+            <h2>Aqui va a ir el form</h2>
+        </div>
+    )
+}
+
+export default PerroForm;
